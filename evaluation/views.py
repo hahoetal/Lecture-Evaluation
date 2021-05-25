119902872190ad4b5a273cdd967c55f8cdd73116
hahoetal <yeong-eun@likelion.org>
2021-05-25 22:21:20 +0900

    강의평 작성, 추천 기능 수정

--- evaluation/views.py
+++ evaluation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required # 데코레이터 사용
+from django.contrib import messages
 
 from .forms import EvalForm
 from accounts.models import User
@@ -11,17 +12,24 @@ from .models import Evals
 @login_required(login_url= '/accounts/login')
 def create(request, num):
     if request.method == "POST":
-        form = EvalForm(request.POST)
-        if form.is_valid():
-            eval = form.save(commit=False) # 일단 DB에 저장하지 않고
-            eval.lect_id = get_object_or_404(Lectures, pk=num) # 강의 정보를 가져와 넣어주고
-            eval.author = User.objects.get(userId=request.user.get_username()) # 글 작성을 요청한 사용자 넣어주고
-            eval.date = timezone.datetime.now() # 현재 시간 넣어주고
-            eval.hw_level = request.POST['hw_level'] # 과제 난이도 넣어주고
-            eval.test_level = request.POST['test_level'] # 시험 난이도 넣어주고
-            eval.lect_power = request.POST['lect_power'] # 강의력까지 넣어주고
-            eval.save() # db에 저장
-            return redirect('/') # 저장되면 강의 자세히 보기 페이지 보여줄 수 있도록 url 수정하기
+        try: # 강의평을 작성하면 일단 요청한 사용자와 강의가 일치하는 강의평 가져오기
+            user = request.user.get_username()
+            lect = Lectures.objects.get(pk=num)
+            Evals.objects.get(author=user, lect_id=lect)
+            messages.warning(request, '강의평을 이미 작성했습니다.')
+        except: # 강의평 가져오기가 실패하면.. 요청한 사용자는 강의평을 작성하지 않은 것 임
+            form = EvalForm(request.POST)
+            if form.is_valid():
+                eval = form.save(commit=False) # 일단 DB에 저장하지 않고
+                eval.lect_id = get_object_or_404(Lectures, pk=num) # 강의 정보를 가져와 넣어주고
+                eval.author = User.objects.get(userId=request.user.get_username()) # 글 작성을 요청한 사용자 넣어주고
+                eval.date = timezone.datetime.now() # 현재 시간 넣어주고
+                eval.hw_level = request.POST['hw_level'] # 과제 난이도 넣어주고
+                eval.test_level = request.POST['test_level'] # 시험 난이도 넣어주고
+                eval.lect_power = request.POST['lect_power'] # 강의력까지 넣어주고
+                eval.save() # db에 저장
+                return redirect('/') # 저장되면 강의 자세히 보기 페이지 보여줄 수 있도록 url 수정하기
+        return redirect('/') # 저장되면 강의 자세히 보기 페이지 보여줄 수 있도록 url 수정하기
     else:
         form = EvalForm()
         return render(request, 'create.html', {'form': form})
@@ -43,7 +51,8 @@ def show_eval(request, lect_id):
 @login_required(login_url= '/accounts/login')
 def recommand(request, eval_num, lect_num):
     eval = get_object_or_404(Evals, pk=eval_num)
-    if request.user.get_username() != eval.author: # 자기가 쓴 글은 추천할 수 없음.
+
+    if request.user != eval.author: # 자기가 쓴 글은 추천할 수 없음.
         eval.count = eval.count + 1 # 해당 함수가 실행되면 count가 1 증가
         eval.save() # 저장을 해주어야만 DB에 반영!!
         
